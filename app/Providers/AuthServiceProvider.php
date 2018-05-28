7141367b41205c3859c7c7d25fcf9a172fda903b
Attila Szeremi <attila@szeremi.org>
2018-05-28 06:54:48 +0200

    Initial commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace amcsi\LyceeOverture\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'amcsi\LyceeOverture\Model' => 'amcsi\LyceeOverture\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace amcsi\LyceeOverture\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'amcsi\LyceeOverture\Events\Event' => [
+            'amcsi\LyceeOverture\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
