b0b890a7b6464674f65dda8a1f34c8b04c518868
Attila Szeremi <attila@szeremi.org>
2018-05-29 14:45:08 +0200

    Create basic card migration

---
+++ app/Card.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace amcsi\LyceeOverture;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    //
+}
---
+++ database/migrations/2018_05_29_112205_create_cards_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use amcsi\LyceeOverture\Card\Type;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->string('id');
+            $table->timestamps();
+            $table->string('variants')->default('');
+            $table->enum('type', Type::getAll());
+            $table->unsignedTinyInteger('ex')->default(0);
+
+            $table->boolean('snow')->default(0);
+            $table->boolean('moon')->default(0);
+            $table->boolean('flower')->default(0);
+            $table->boolean('space')->default(0);
+            $table->boolean('sun')->default(0);
+
+            $table->boolean('cost_star')->default(0);
+            $table->boolean('cost_snow')->default(0);
+            $table->boolean('cost_moon')->default(0);
+            $table->boolean('cost_flower')->default(0);
+            $table->boolean('cost_space')->default(0);
+            $table->boolean('cost_sun')->default(0);
+
+            $table->unsignedTinyInteger('ap')->default(0);
+            $table->unsignedTinyInteger('dp')->default(0);
+            $table->unsignedTinyInteger('sp')->default(0);
+            $table->unsignedTinyInteger('dmg')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
