5464071edcda48dfb3eac9ebc765ff4cf42f6fe8
Attila Szeremi <attila@szeremi.org>
2018-06-04 21:05:21 +0200

    Separate auto-translating of stat changes into separate class

--- app/I18n/AutoTranslator.php
+++ app/I18n/AutoTranslator.php
@@ -6,6 +6,7 @@ namespace amcsi\LyceeOverture\I18n;
 use amcsi\LyceeOverture\I18n\AutoTranslator\DiscardFromDeck;
 use amcsi\LyceeOverture\I18n\AutoTranslator\DrawCards;
 use amcsi\LyceeOverture\I18n\AutoTranslator\FullWidthCharacters;
+use amcsi\LyceeOverture\I18n\AutoTranslator\StatChanges;
 use amcsi\LyceeOverture\I18n\AutoTranslator\Target;
 use amcsi\LyceeOverture\I18n\AutoTranslator\WhenAppears;
 use amcsi\LyceeOverture\I18n\AutoTranslator\WhenSupporting;
@@ -43,72 +44,7 @@ class AutoTranslator
         );
 
         // "... get $statChanges."
-        $autoTranslated = preg_replace_callback(
-            '/((?:(味方|相手|この)((?:\[.+?\])*))?キャラ((\d)体|全て)?|})に((?:(?:AP|DP|SP|DMG)[+-]\d(?:, )?)+)する./u',
-            function ($matches) use ($autoTranslated) {
-                $plural = false;
-                $target = $matches[1] === '}'; // Whether the effect targets.
-                $subject = $matches[2]; // Ally or Enemy in Japanese (or '')
-                $something = $matches[3]; // e.g. [sun] <- characters
-                $allOrHowMany = $matches[4];
-                $all = $allOrHowMany === '全て';
-                $howMany = $matches[5];
-                $statChanges = $matches[6]; // The stat changes involved.
-                if (!$target) {
-                    if ($all) {
-                        switch ($subject) {
-                            case '味方':
-                                $text = "all your $something characters";
-                                break;
-                            case '相手':
-                                $text = "all enemy $something characters";
-                                break;
-                            case '':
-                                // Unknown
-                                $text = "all $something characters";
-                                break;
-                            default:
-                                throw new \LogicException("Unexpected all subject: $subject");
-                        }
-                        $plural = true;
-                    } else {
-                        switch ($subject) {
-                            case '味方':
-                                $text = 'ally';
-                                break;
-                            case '相手':
-                                $text = 'enemy';
-                                break;
-                            case 'この':
-                                $text = 'this';
-                                break;
-                            case '':
-                                // Unknown
-                                $text = '';
-                                break;
-                            default:
-                                throw new \LogicException("Unexpected subject: $subject");
-                        }
-                        $text = "$text $something character";
-                        if ($howMany > 1) {
-                            $plural = true;
-                            $text = "$howMany ${text}s";
-                        }
-                    }
-                    $text = " $text";
-                } else {
-                    $text = '}';
-                }
-                $s = $plural ? '' : 's';
-                return sprintf(
-                    '%s get%s %s.',
-                    $text,
-                    $s,
-                    $statChanges
-                );
-            },
-            $autoTranslated
-        );
+        $autoTranslated = StatChanges::autoTranslate($autoTranslated);
 
         // "... gain $basicAbility."
         $autoTranslated = preg_replace_callback(
