a4ea5bd94e49cf08a0a7fda19123cbafaf53daaa
Attila Szeremi <attila@szeremi.org>
2018-07-14 16:04:51 +0200

    Create card_sets table

---
+++ database/migrations/2018_07_14_133856_create_card_sets.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardSets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'card_sets',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name_jp');
+                $table->string('name_en')->default('');
+                $table->string('cards')->string();
+                $table->boolean('deck');
+                $table->timestamps();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('card_sets');
+    }
+}
