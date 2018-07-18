c3e5e818fdbc8ec4ce18ddb2f4791d2d181f749e
Attila Szeremi <attila@szeremi.org>
2018-07-18 11:16:20 +0200

    Add hidden text to icons for copy-pastability

--- resources/assets/js/utils/formatCard.js
+++ resources/assets/js/utils/formatCard.js
@@ -1,5 +1,10 @@
-function getIcon(name) {
-  return `<span class="card-text-icon card-text-icon-${name}"></span>`;
+/**
+ * @param name
+ * @param text The hidden text for when copy-pasting.
+ * @return {string}
+ */
+function getIcon(name, text) {
+  return `<span class="card-text-icon card-text-icon-${name}">${text}</span>`;
 }
 
 /**
@@ -8,7 +13,7 @@ function getIcon(name) {
 export default function(text = '') {
   text = text.replace(/\[T]/g, getIcon('tap'));
   text = text.replace(/\[(0|star|snow|moon|flower|space|sun)]/g, (match, contents) => {
-    return getIcon(contents);
+    return getIcon(contents, match);
   });
   return text;
 }
