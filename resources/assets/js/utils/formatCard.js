f04f092acd3348ab5beb2624724c40d3fbe6f708
Attila Szeremi <attila@szeremi.org>
2018-07-18 10:28:01 +0200

    Use tap icon

--- resources/assets/js/utils/formatCard.js
+++ resources/assets/js/utils/formatCard.js
@@ -1,6 +1,12 @@
+function getIcon(name) {
+  return `<span class="card-text-icon card-text-icon-${name}"></span>`;
+}
+
 /**
  * Formats card text to use pretty icons and stuff.
  */
 export default function(text = '') {
-  return text.replace(/\[0]/g, '<span class="card-text-icon card-text-icon-free"></span>');
+  text = text.replace(/\[0]/g, getIcon('free'));
+  text = text.replace(/\[T]/g, getIcon('tap'));
+  return text;
 }
