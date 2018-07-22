aa3a6f1c4cde67dc33020a61989036560c64ab7a
Attila Szeremi <attila@szeremi.org>
2018-07-22 21:17:00 +0200

    Add icons for [D1]...[D4]

--- resources/assets/js/utils/formatCard.js
+++ resources/assets/js/utils/formatCard.js
@@ -7,13 +7,20 @@ function getIcon(name, text) {
   return `<span class="card-text-icon card-text-icon-${name}">${text}</span>`;
 }
 
+function simpleCallback(match, contents) {
+  return getIcon(contents, match);
+}
+
+function discardCallback(match, contents) {
+  return getIcon('d' + contents, match);
+}
+
 /**
  * Formats card text to use pretty icons and stuff.
  */
 export default function(text = '') {
   text = text.replace(/\[T]/g, getIcon('tap'));
-  text = text.replace(/\[(0|star|snow|moon|flower|space|sun)]/g, (match, contents) => {
-    return getIcon(contents, match);
-  });
+  text = text.replace(/\[(0|star|snow|moon|flower|space|sun)]/g, simpleCallback);
+  text = text.replace(/\[D([1-4])]/g, discardCallback);
   return text;
 }
