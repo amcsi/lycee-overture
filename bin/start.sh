d3be4dc35febe622808bbb8d511c95b57db04b2c
Attila Szeremi <attila@szeremi.org>
2018-07-23 12:31:51 +0200

    Do migration on start, and import all in the background

--- bin/start.sh
+++ bin/start.sh
@@ -2,4 +2,11 @@
 # Remove old pid file if it exists.
 rm -f storage/logs/swoole_http.pid
 
+# Migrate the DB.
+php artisan migrate
+
+# Do the import tasks (in the background).
+php artisan lycee:import-all &
+
+# Run the Swoole HTTP server.
 php artisan swoole:http start
