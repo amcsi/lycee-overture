0da8a155629ace8c4c237bef817332980f3f136f
Attila Szeremi <attila@szeremi.org>
2018-07-25 10:29:38 +0200

    Change color of basic abilities

--- resources/assets/js/components/card/CardDescription.vue
+++ resources/assets/js/components/card/CardDescription.vue
@@ -1,5 +1,13 @@
 <template>
-    <span v-html="cardHtml"></span>
+    <span>
+        <span v-if="basicAbilities" class="basic-abilities" v-html="basicAbilities"></span>
+        <br v-if="basicAbilities" />
+        <span v-if="abilityCost" class="ability-cost" v-html="abilityCost"></span>
+        <span v-if="abilityCost">: </span>
+        <span class="ability-description" v-html="abilityDescription"></span>
+        <br v-if="comments" />
+        <span v-if="comments" class="comments" v-html="comments"></span>
+    </span>
 </template>
 
 <script>
@@ -12,26 +20,25 @@
       translation: { required: true },
     },
     computed: {
-      cardHtml() {
-        const { ability_cost, ability_description, basic_abilities, comments } = this.translation;
-
-        let text = '';
-        if (basic_abilities) {
-          text += `<span class="basic_abilities">${formatCardText(basic_abilities)}</span><br />`;
-        }
-        if (ability_cost) {
-          text += `<span>${formatCardText(ability_cost)}</span>: `;
-        }
-        text += formatCardText(ability_description);
-        if (comments) {
-          text += `<br /><span class="comments">${comments}</span>`;
-        }
-        return text;
+      basicAbilities() {
+        return formatCardText(this.translation.basic_abilities);
+      },
+      abilityCost() {
+        return formatCardText(this.translation.ability_cost);
+      },
+      abilityDescription() {
+        return formatCardText(this.translation.ability_description);
+      },
+      comments() {
+        return formatCardText(this.translation.comments);
       },
     },
+    methods: {},
   };
 </script>
 
 <style scoped>
-
+    .basic-abilities {
+        color: #49a97b;
+    }
 </style>
