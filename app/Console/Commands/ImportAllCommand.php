c6b389f813062ed1b19889a3501f2f02c85964b7
Attila Szeremi <attila@szeremi.org>
2018-07-25 13:02:14 +0200

    Include option to download-upload images in import all command; +StopWatch

--- app/Console/Commands/ImportAllCommand.php
+++ app/Console/Commands/ImportAllCommand.php
@@ -3,18 +3,36 @@ declare(strict_types=1);
 
 namespace amcsi\LyceeOverture\Console\Commands;
 
+use amcsi\LyceeOverture\Debug\Profiling;
 use Illuminate\Console\Command;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 class ImportAllCommand extends Command
 {
-    protected $signature = 'lycee:import-all';
+    public const COMMAND = 'lycee:import-all';
+
+    protected $signature = self::COMMAND . ' {--images : Also download images from website and upload to cloudinary}';
+
     protected $description = 'Does importing of the CSV, its data, and auto translations.';
 
     public function handle()
     {
+        $stopwatch = new Stopwatch();
+        $stopwatchEvent = $stopwatch->start('import-all');
+        $this->output->text('Started doing all the import tasks...');
+
         $this->call(DownloadCsvCommand::COMMAND);
         $this->call(ImportBasicCardsCommand::COMMAND);
         $this->call(ImportTextsCommand::COMMAND);
         $this->call(AutoTranslateCommand::COMMAND);
+
+        if ($this->option('images')) {
+            $this->call(ImageDownloadCommand::COMMAND);
+            $this->call(ImageUploadCommand::COMMAND);
+        }
+
+        $this->output->text(
+            "Finished doing all the import tasks in " . Profiling::stopwatchToHuman($stopwatchEvent->stop())
+        );
     }
 }
