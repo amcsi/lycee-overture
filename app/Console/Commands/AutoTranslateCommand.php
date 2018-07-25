f2ad4d52f5b419b860ca91e16ebdc9f09acea647
Attila Szeremi <attila@szeremi.org>
2018-07-25 10:59:19 +0200

    Include basic abilities in auto translation

--- app/Console/Commands/AutoTranslateCommand.php
+++ app/Console/Commands/AutoTranslateCommand.php
@@ -18,7 +18,7 @@ use Illuminate\Database\Query\Builder;
 class AutoTranslateCommand extends Command
 {
     public const COMMAND = 'lycee:auto-translate';
-    public const AUTO_TRANSLATE_FIELDS = ['ability_description'];
+    public const AUTO_TRANSLATE_FIELDS = ['ability_description', 'basic_abilities'];
 
     protected $signature = self::COMMAND;
 
