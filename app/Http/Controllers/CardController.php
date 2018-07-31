3739a7b39e5c5f89fc9c0dfd3fc335c03204d9dd
Attila Szeremi <attila@szeremi.org>
2018-07-31 20:47:00 +0100

    Show translated cards first in the list

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -1,15 +1,27 @@
 <?php
+declare(strict_types=1);
 
 namespace amcsi\LyceeOverture\Http\Controllers;
 
 use amcsi\LyceeOverture\Card;
 use amcsi\LyceeOverture\Card\CardTransformer;
+use Illuminate\Database\Query\JoinClause;
 
 class CardController extends Controller
 {
     public function index(CardTransformer $cardTransformer)
     {
-        $cards = Card::paginate(50);
+        $cards = Card::select(['cards.*'])
+            ->join(
+                'card_translations as t',
+                function (JoinClause $join) {
+                    $join->on('cards.id', '=', 't.card_id')
+                        ->where('t.locale', '=', 'en');
+                }
+            )
+            ->orderBy('t.kanji_count', 'asc')
+            ->orderBy('id', 'asc')
+            ->paginate(50);
         return $this->response->paginator($cards, $cardTransformer);
     }
 }
