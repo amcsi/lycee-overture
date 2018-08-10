f1a919e0c090a5abc9518cdf6c59aea733a66f2b
Attila Szeremi <attila@szeremi.org>
2018-08-10 11:30:36 +0200

    Show loaders for async components

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -3,8 +3,30 @@
  */
 
 import VueRouter from 'vue-router';
-const CardList = () => import('./components/CardList');
-const IndexPage = () => import('./pages/IndexPage');
+
+/**
+ * Loading wrapper for code-split async components in the route.
+ */
+const loadingizeAsyncComponent = asyncComponent => () => ({
+  // The component to load (should be a Promise)
+  component: asyncComponent(),
+  // A component to use while the async component is loading
+  loading: {
+    render(h) {
+      return h('div', {directives: [{ loading: true }]});
+    },
+  },
+  // A component to use if the load fails
+  error: null,
+  // Delay before showing the loading component. Default: 200ms.
+  delay: 200,
+  // The error component will be displayed if a timeout is
+  // provided and exceeded. Default: Infinity.
+  timeout: Infinity,
+});
+
+const CardList = loadingizeAsyncComponent(() => import('./components/CardList'));
+const IndexPage = loadingizeAsyncComponent(() => import ('./pages/IndexPage'));
 
 const router = new VueRouter({
   mode: 'history', // HTML5 history.
