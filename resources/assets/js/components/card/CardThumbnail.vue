amcsi/lycee-overture

commit 3c857dbb064fc300153f5e3b823c25ffdd7988b6

Avoid JS console error because of this.poppers not existing

resources/assets/js/components/card/CardThumbnail.vue
@@ -48,6 +48,9 @@
       },
     },
     components: { CardImage },
+    created() {
+      this.poppers = [];
+    },
     methods: {
       setupPopper() {
         const reference = this.$el.closest('td');

commit b20e3804435227346d0431f61b79a929fe57259f

Fix thumbnail poppers broken by 3c857db

resources/assets/js/components/card/CardThumbnail.vue
@@ -48,9 +48,6 @@
       },
     },
     components: { CardImage },
-    created() {
-      this.poppers = [];
-    },
     methods: {
       setupPopper() {
         const reference = this.$el.closest('td');
@@ -77,7 +74,9 @@
       },
     },
     destroyed() {
-      this.poppers.forEach(popper => popper.destroy());
+      if (this.poppers) {
+        this.poppers.forEach(popper => popper.destroy());
+      }
     },
   };
 </script>