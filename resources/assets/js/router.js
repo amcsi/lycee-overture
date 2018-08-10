323d3652c7f368719427c614290a9de298e43128
Attila Szeremi <attila@szeremi.org>
2018-08-10 11:30:36 +0200

    Use Async components, but lose code splitting

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -3,8 +3,27 @@
  */
 
 import VueRouter from 'vue-router';
-const CardList = () => import('./components/CardList');
-const IndexPage = () => import('./pages/IndexPage');
+
+const asyncComponentize = componentPromise => () => ({
+  // The component to load (should be a Promise)
+  component: componentPromise,
+  // A component to use while the async component is loading
+  loading: {
+    render(h) {
+      return h('div', {directives: [{ loading: true }]});
+    },
+  },
+  // A component to use if the load fails
+  error: null,
+  // Delay before showing the loading component. Default: 200ms.
+  delay: 200,
+  // The error component will be displayed if a timeout is
+  // provided and exceeded. Default: Infinity.
+  timeout: Infinity,
+});
+
+const CardList = asyncComponentize(import('./components/CardList'));
+const IndexPage = asyncComponentize(import ('./pages/IndexPage'));
 
 const router = new VueRouter({
   mode: 'history', // HTML5 history.
