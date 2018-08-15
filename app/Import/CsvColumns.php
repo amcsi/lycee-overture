110954f85b820c63cce4ee11d683bcce2e0aebc0
Attila Szeremi <attila@szeremi.org>
2018-08-15 22:54:38 +0200

    Adjust csv columns constants to new CSV format

--- app/Import/CsvColumns.php
+++ app/Import/CsvColumns.php
@@ -16,13 +16,15 @@ class CsvColumns
     public const ELEMENTS = 5;
     public const EX = 6;
     public const COST = 7;
-    public const AP = 8;
-    public const DP = 9;
-    public const SP = 10;
-    public const DMG = 11;
-    public const CHARACTER_TYPE = 12;
-    public const BASIC_ABILITIES = 13;
-    public const ABILITY = 14;
-    public const CARD_SET = 15;
-    public const CARD_SET_2 = 16;
+    public const POSITION_FLAGS = 8;
+    public const AP = 9;
+    public const DP = 10;
+    public const SP = 11;
+    public const DMG = 12;
+    public const CHARACTER_TYPE = 13;
+    public const TEAM = 14;
+    public const BASIC_ABILITIES = 15;
+    public const ABILITY = 16;
+    public const CARD_SET = 17;
+    public const CARD_SET_2 = 18;
 }
