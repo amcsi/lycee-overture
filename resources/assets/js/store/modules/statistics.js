6d814e849a717c2e40221c13b57f5b1e4232c101
Attila Szeremi <attila@szeremi.org>
2018-09-01 19:42:52 +0200

    Do not reload statistics when only the page changes

--- resources/assets/js/store/modules/statistics.js
+++ resources/assets/js/store/modules/statistics.js
@@ -3,6 +3,7 @@ import api from '../../api';
 export default {
   namespaced: true,
   state: {
+    lastStatisticsParams: null,
     statisticsLoading: false,
     statistics: {},
   },
@@ -17,15 +18,28 @@ export default {
     STATISTICS_LOADING_FAILED(state) {
       state.statisticsLoading = false;
     },
+    STATISTICS_SET_LAST_PARAMS(state, stringifiedParams) {
+      state.lastStatisticsParams = stringifiedParams;
+    },
   },
   actions: {
-    async fetchStatistics({ commit }, params) {
-      commit('STATISTICS_LOADING');
-      try {
-        const statistics = (await api.get('/statistics', { params })).data.data;
-        commit('STATISTICS_LOADING_SUCCEEDED', statistics);
-      } catch (e) {
-        commit('STATISTICS_LOADING_FAILED');
+    async fetchStatistics({ commit, state }, query) {
+      const params = { ...query };
+      // The page doesn't matter when getting statistics.
+      delete params.page;
+
+      const stringifiedParams = JSON.stringify(params);
+
+      // If e.g. only a page change happened, then the statistics would not need to be reloaded.
+      if (stringifiedParams !== state.lastStatisticsParams) {
+        commit('STATISTICS_LOADING');
+        try {
+          const statistics = (await api.get('/statistics', { params })).data.data;
+          commit('STATISTICS_SET_LAST_PARAMS', stringifiedParams);
+          commit('STATISTICS_LOADING_SUCCEEDED', statistics);
+        } catch (e) {
+          commit('STATISTICS_LOADING_FAILED');
+        }
       }
     },
   },
