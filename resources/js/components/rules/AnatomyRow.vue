0fc072cc2a533f88ed4b651703dfdfdcff62efe8
Attila Szeremi <attila@szeremi.org>
2018-11-08 19:19:06 +0100

    Add character card anatomy table

---
+++ resources/js/components/rules/AnatomyRow.vue
@@ -0,0 +1,60 @@
+<template>
+    <tr>
+        <td class="number-and-name-column">
+            <div class="number-and-name-container">
+                <img
+                    :src="`https://lycee-tcg.com/rule/images/index_2_number${number}.jpg`"
+                    alt="$t('rules.characterCard.numberPointingToCharacterImage')"
+                />
+
+                <div class="number-and-name--name">
+                    {{ name }}
+                </div>
+            </div>
+        </td>
+        <td>
+            <slot>(description goes here)</slot>
+        </td>
+    </tr>
+</template>
+
+<script>
+  /** @class AnatomyRow */
+  export default {
+    name: 'AnatomyRow',
+    props: {
+      number: {
+        type: [Number, String],
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style scoped lang="scss">
+
+    .number-and-name-container {
+        display: flex;
+        align-items: flex-start;
+
+        img {
+            margin-right: .5em;
+            vertical-align: top;
+        }
+    }
+
+    .number-and-name--name {
+        padding-top: .2em;
+    }
+
+    td {
+        border: 1px solid gray;
+        padding: .3em;
+
+        line-height: 1.5;
+    }
+</style>
--- resources/js/pages/RulesPage.vue
+++ resources/js/pages/RulesPage.vue
@@ -20,16 +20,74 @@
             {{ $t('rules.characterCard.overview') }}<br />
             {{ $t('rules.characterCard.overviewNote') }}
         </p>
+
+        <div class="character-anatomy-container">
+            <img src="https://lycee-tcg.com/rule/images/index_2_img1.png" :alt="$t('rules.characterCard.imageAlt')" />
+            <AnatomyTable>
+                <AnatomyRow number="1" :name="$t('rules.characterCard.cardName')">
+                    {{ $t('rules.characterCard.cardNameDescription') }}
+                </AnatomyRow>
+                <AnatomyRow number="2" :name="$t('rules.characterCard.element')">
+                    {{ $t('rules.characterCard.elementDescription') }}
+                </AnatomyRow>
+                <AnatomyRow number="3" :name="$t('rules.characterCard.ex')">
+                    {{ $t('rules.characterCard.exDescription') }}
+                </AnatomyRow>
+                <AnatomyRow number="4" :name="$t('rules.characterCard.cost')">
+                    {{ $t('rules.characterCard.costDescription') }}
+                </AnatomyRow>
+                <AnatomyRow number="5" :name="$t('rules.characterCard.positionRestriction')">
+                    {{ $t('rules.characterCard.positionRestrictionDescription') }}
+                </AnatomyRow>
+                <AnatomyRow number="6" name="AP">
+                    {{ $t('rules.characterCard.apDescription') }}
+                </AnatomyRow>
+                <AnatomyRow number="7" name="DMG">
+                    {{ $t('rules.characterCard.dmgDescription') }}
+                </AnatomyRow>
+                <AnatomyRow number="8" name="DP">
+                    {{ $t('rules.characterCard.dpDescription') }}
+                </AnatomyRow>
+                <AnatomyRow number="9" name="SP">
+                    {{ $t('rules.characterCard.spDescription') }}
+                </AnatomyRow>
+                <AnatomyRow number="10" :name="$t('rules.characterCard.basicAbility')">
+                    {{ $t('rules.characterCard.basicAbilityDescription') }}
+                </AnatomyRow>
+                <AnatomyRow number="11" :name="$t('rules.characterCard.specialAbility')">
+                    {{ $t('rules.characterCard.specialAbilityDescription') }}
+                </AnatomyRow>
+                <AnatomyRow number="12" :name="$t('rules.characterCard.cardNumber')">
+                    {{ $t('rules.characterCard.cardNumberDescription') }}<br />
+                    {{ $t('rules.characterCard.cardNumberLettersExplanation') }}
+                </AnatomyRow>
+                <AnatomyRow number="13" :name="$t('rules.characterCard.type')">
+                    {{ $t('rules.characterCard.typeDescription1') }}<br />
+                    {{ $t('rules.characterCard.typeDescription2') }}
+                </AnatomyRow>
+                <AnatomyRow number="14" :name="$t('rules.characterCard.brand')">
+                    {{ $t('rules.characterCard.brandDescription1') }}<br />
+                    {{ $t('rules.characterCard.brandDescription2') }}
+                </AnatomyRow>
+            </AnatomyTable>
+        </div>
     </div>
 </template>
 
 <script>
+  import AnatomyRow from '../components/rules/AnatomyRow';
+  import AnatomyTable from '../components/rules/AnatomyTable';
+
   /** @class RulesPage */
   export default {
     name: 'RulesPage',
+    components: { AnatomyRow, AnatomyTable },
   };
 </script>
 
 <style scoped>
-
+    .character-anatomy-container {
+        display: flex;
+        align-items: flex-start;
+    }
 </style>
