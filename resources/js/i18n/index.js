237e1eff4c5fe625255212f55e303c5ad80ab3eb
Attila Szeremi <attila@szeremi.org>
2018-11-10 15:05:47 +0100

    Hot reload language js files

--- resources/js/i18n/index.js
+++ resources/js/i18n/index.js
@@ -11,4 +11,11 @@ const i18n = new VueI18n({
   },
 });
 
+// Hot updates
+if (module.hot) {
+  module.hot.accept('./en', function () {
+    i18n.setLocaleMessage('en', require('./en').default)
+  })
+}
+
 export default i18n;
