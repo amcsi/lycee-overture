8ee33488464770ea1f80cd9a7b7baa0d96b277ab
Attila Szeremi <attila@szeremi.org>
2018-11-10 13:56:30 +0100

    Complete cost rules section

--- resources/js/components/rules/CostRules.vue
+++ resources/js/components/rules/CostRules.vue
@@ -23,21 +23,178 @@
             <AnatomyRow only-name number="3" :name="$t('rules.costCard.cost')" />
         </ImageWithAnatomy>
 
-        <p>In progress...</p>
+        <p>
+            <CardText :text="$t('rules.costExample.example1')"></CardText>
+        </p>
+
+        <p>
+            <img src="https://lycee-tcg.com/rule/images/index_4_img2.png" :alt="$t('rules.costExample.imageAlt')" />
+        </p>
+
+        <h3>{{ $t('rules.howToSummon.title') }}</h3>
+
+        <p>
+            <CardText :text="$t('rules.howToSummon.description')"></CardText>
+        </p>
+
+        <p>
+            <img src="https://lycee-tcg.com/rule/images/index_4_img3.png" :alt="$t('rules.howToSummon.imageAlt')" />
+        </p>
+
+        <el-card class="card">
+            <h4>
+                <CardText :text="$t('rules.howToSummon.box.title')" />
+            </h4>
+
+            <p class="text-center">
+                ①
+                <CardText :text="$t('rules.howToSummon.box.option1')" />
+                <br />
+                {{ $t('rules.howToSummon.box.or') }}<br />
+                ②
+                <CardText :text="$t('rules.howToSummon.box.option2')" />
+            </p>
+        </el-card>
+
+        <div class="arrow-pointing-down"></div>
+
+        <div class="discard2-box">
+            ①
+            <CardText :text="$t('rules.howToSummon.discard2Box')" />
+        </div>
+
+        <p>{{ $t('rules.howToSummon.box.or') }} ...</p>
+
+        <div class="discard1-box">
+            ②
+            <CardText :text="$t('rules.howToSummon.discard1Box')" />
+        </div>
+
+        <h3>{{ $t('rules.typesOfCosts.title') }}</h3>
+
+        <table class="type-of-costs-table">
+            <tr>
+                <td>
+                    <CardText text="[snow]" />
+                </td>
+                <td>
+                    <CardText :text="$t('rules.typesOfCosts.oneCostOfSnow')" />
+                </td>
+                <td>
+                    <CardText text="[moon]" />
+                </td>
+                <td>
+                    <CardText :text="$t('rules.typesOfCosts.oneCostOfMoon')" />
+                </td>
+                <td>
+                    <CardText text="[flower]" />
+                </td>
+                <td>
+                    <CardText :text="$t('rules.typesOfCosts.oneCostOfFlower')" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <CardText text="[space]" />
+                </td>
+                <td>
+                    <CardText :text="$t('rules.typesOfCosts.oneCostOfSpace')" />
+                </td>
+                <td>
+                    <CardText text="[sun]" />
+                </td>
+                <td>
+                    <CardText :text="$t('rules.typesOfCosts.oneCostOfSun')" />
+                </td>
+                <td>
+                    <CardText text="[star]" />
+                </td>
+                <td>{{ $t('rules.typesOfCosts.oneCostOfStar') }}</td>
+            </tr>
+            <tr>
+                <td>
+                    <CardText text="[T]" />
+                </td>
+                <td colspan="5">{{ $t('rules.typesOfCosts.tap') }}</td>
+            </tr>
+            <tr>
+                <td>
+                    <CardText text="[D1]" />
+                </td>
+                <td colspan="5">{{ $t('rules.typesOfCosts.tap') }}</td>
+            </tr>
+            <tr>
+                <td>{{ $t('rules.typesOfCosts.other.title') }}</td>
+                <td colspan="5">{{ $t('rules.typesOfCosts.other.description') }}</td>
+            </tr>
+            <tr>
+                <td>
+                    <CardText text="[0]" />
+                </td>
+                <td colspan="5">{{ $t('rules.typesOfCosts.free') }}</td>
+            </tr>
+        </table>
+
+        <h3>{{ $t('rules.assist.title') }}</h3>
+
+        <p>{{ $t('rules.assist.description') }}</p>
     </div>
 </template>
 
 <script>
+  import CardText from '../card/CardText';
   import AnatomyRow from './AnatomyRow';
   import ImageWithAnatomy from './ImageWithAnatomy';
 
   /** @class CostRules */
   export default {
     name: 'CostRules',
-    components: { AnatomyRow, ImageWithAnatomy },
+    components: { CardText, AnatomyRow, ImageWithAnatomy },
   };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .card {
+        text-align: center;
+    }
 
+    .arrow-pointing-down {
+        background: url(https://lycee-tcg.com/rule/images/index_4_img_aicon3.jpg) no-repeat center;
+        width: 700px;
+        height: 100px;
+    }
+
+    .discard1-box, .discard2-box {
+        background-repeat: no-repeat;
+        padding: 15px 0 0 0;
+        width: 677px;
+        height: 409px;
+        font-size: 20px;
+        color: #FFFFFF;
+        text-align: center;
+    }
+
+    .discard2-box {
+        background-image: url(https://lycee-tcg.com/rule/images/index_4_img4.jpg);
+    }
+
+    .discard1-box {
+        background-image: url(https://lycee-tcg.com/rule/images/index_4_img5.jpg);
+    }
+
+    .type-of-costs-table {
+        border-collapse: collapse;
+    }
+
+    .type-of-costs-table {
+        td {
+            border: 1px solid gray;
+            padding: .5em;
+
+        }
+
+        td:first-child {
+            text-align: center;
+        }
+    }
 </style>
--- resources/js/i18n/en/index.js
+++ resources/js/i18n/en/index.js
@@ -1,10 +1,12 @@
 import cardFilters from './cardFilters';
+import game from './game';
 import nav from './nav';
 import rules from './rules';
 import welcome from './welcome';
 
 export default {
   cardFilters,
+  game,
   nav,
   rules,
   welcome,
