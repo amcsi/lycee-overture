ff674fcd5242d5f3b5073f0ccaf4cdbe4e904846
Attila Szeremi <attila@szeremi.org>
2018-12-23 11:36:08 +0100

    Improve YahooRawKanjiTranslatorTest's testTranslate()

--- tests/Unit/I18n/TranslatorApi/YahooRawKanjiTranslatorTest.php
+++ tests/Unit/I18n/TranslatorApi/YahooRawKanjiTranslatorTest.php
@@ -20,18 +20,13 @@ class YahooRawKanjiTranslatorTest extends TestCase
     {
         $apiKey = 'whatever';
         $kanjiInput = '左衛門佐';
-        $guzzleClient = \Mockery::mock(Client::class);
-        $options = [
-            'query' => [
-                'appid' => $apiKey,
-                'sentence' => $kanjiInput,
-            ],
-        ];
-        $guzzleClient->expects()
-            ->get('https://jlp.yahooapis.jp/FuriganaService/V1/furigana', $options)
-            ->andReturn(
-                new Response(200, [], file_get_contents(__DIR__ . '/result.xml'))
-            );
+
+        $guzzleClient = new Client(['handler' => HandlerStack::create(new MockHandler([
+            function (Request $request) use ($apiKey, $kanjiInput) {
+                self::assertCorrectRequestUrl($request, $apiKey, $kanjiInput);
+                return new Response(200, [], file_get_contents(__DIR__ . '/result.xml'));
+            },
+        ]))]);
 
         $instance = new YahooRawKanjiTranslator($guzzleClient, $apiKey);
 
@@ -44,10 +39,7 @@ class YahooRawKanjiTranslatorTest extends TestCase
         $kanjiInput = 'bad_kanji';
         $guzzleClient = new Client(['handler' => HandlerStack::create(new MockHandler([
             function (Request $request) use ($apiKey, $kanjiInput) {
-                self::assertSame(
-                    "https://jlp.yahooapis.jp/FuriganaService/V1/furigana?appid=$apiKey&sentence=$kanjiInput",
-                    (string) $request->getUri()
-                );
+                self::assertCorrectRequestUrl($request, $apiKey, $kanjiInput);
                 return new Response(503, [], 'bla bla bla invalid parameter: sentence bla');
             },
         ]))]);
@@ -55,5 +47,14 @@ class YahooRawKanjiTranslatorTest extends TestCase
         $instance = new YahooRawKanjiTranslator($guzzleClient, $apiKey);
 
         self::assertSame($kanjiInput, $instance->translate($kanjiInput));
+    }
+
+    private static function assertCorrectRequestUrl(Request $request, string $apiKey, string $kanjiInput): void
+    {
+        $encodedKanjiInput = rawurlencode($kanjiInput);
+        self::assertSame(
+            "https://jlp.yahooapis.jp/FuriganaService/V1/furigana?appid=$apiKey&sentence=$encodedKanjiInput",
+            (string) $request->getUri()
+        );
     }
 }
