8c29a35e354cbfbf99dca6f9697e2f1c45f0280b
Attila Szeremi <attila@szeremi.org>
2019-02-06 11:12:41 +0100

    Try to upgrade to Laravel Mix 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-  .extract(['vue', 'vue-router', 'vuex', 'element-ui', 'vue-i18n'])
+  .extract()
 ;
 
 if (!mix.inProduction()) {
