b96528dd833aee69bb4939e534774d1a000406c3
Attila Szeremi <attila@szeremi.org>
2019-04-07 18:04:33 +0200

    Add language selectors to right corner of the nav bar

--- resources/js/components/NavMenu.vue
+++ resources/js/components/NavMenu.vue
@@ -9,16 +9,59 @@
         <el-menu-item index="/cards">
             <router-link to="/cards">{{ $t('nav.cardList') }}</router-link>
         </el-menu-item>
+
+        <li class="language-links">
+            <a :href="enHref" class="language-link" :class="{ active: locale !== 'ja' }">
+                <img src="../../images/flags/gb.png" alt="English" title="English" />
+            </a>
+            <a :href="jaHref" class="language-link" :class="{ active: locale === 'ja' }">
+                <img src="../../images/flags/jp.png" alt="日本語" title="日本語" />
+            </a>
+        </li>
     </el-menu>
 </template>
 
 <script>
+function getLocaleChangeUrl(vueComponent, locale) {
+  let url = vueComponent.$route.fullPath;
+  url += url.indexOf('?') >= 0 ? '&' : '?';
+  url += `locale=${locale}`;
+  return url;
+}
+
   /** @class NavMenu */
   export default {
     components: {},
+    data() {
+      return {
+        locale: window.locale,
+      };
+    },
+    computed: {
+      enHref() {
+        return getLocaleChangeUrl(this, 'en');
+      },
+      jaHref() {
+        return getLocaleChangeUrl(this, 'ja');
+      },
+    },
   };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.language-links {
+    float: right;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 
+.language-link {
+    margin-left: 0.5em;
+
+    &:not(.active) {
+        opacity: 0.25;
+    }
+}
 </style>
