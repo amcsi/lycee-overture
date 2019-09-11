f4b0c36745fe13a644c9bd228bc43c33a44912d0
Attila Szeremi <attila@szeremi.org>
2019-09-11 13:07:55 +0200

    Add tool to add timestamp fields to a table migration

---
+++ database/tools/Tools.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace database\migrations\tools;
+
+use Illuminate\Database\Schema\Blueprint;
+
+class Tools
+{
+    /**
+     * Creates creation and update timestamps where the update one auto-updates
+     * regardless of the MySQL explicit_defaults_for_timestamp setting.
+     */
+    public static function timestamps(Blueprint $table, $precision = 0)
+    {
+        $table->timestamp('updated_at', $precision)
+            ->default(\DB::raw("CURRENT_TIMESTAMP($precision) on update CURRENT_TIMESTAMP($precision)"));
+        $table->timestamp('created_at', $precision)->default(\DB::raw("CURRENT_TIMESTAMP($precision)"));
+    }
+}
