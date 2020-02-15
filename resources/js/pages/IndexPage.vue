df15e8293afaefa08d9d1e2adc5ea962d0800e4f
Attila Szeremi <attila@szeremi.org>
2020-02-15 14:11:53 +0100

    Update links on Welcome page

--- resources/js/pages/IndexPage.vue
+++ resources/js/pages/IndexPage.vue
@@ -32,16 +32,26 @@
                 <ExternalLink href="https://discord.gg/xtcQDtQ">
                     <img
                         class="discord-icon"
+                        alt="Discord"
                         src="https://discordapp.com/assets/f8389ca1a741a115313bede9ac02e2c0.svg"
                     >
                     Discord server
                 </ExternalLink>
             </li>
             <li>
-                <ExternalLink href="https://lycee-tcg.com/">lycee-tcg.com - Japanese Official Lycee Overture website</ExternalLink>
+                <ExternalLink href="https://lycee-tcg.eu/storage/lackey/lycee-overture-translated/updatelist.txt">
+                    LackeyCCG auto-updating plugin (work in progress)
+                </ExternalLink>
             </li>
             <li>
-                <ExternalLink href="http://akiba.geocities.jp/lycee_tcg/">akiba.geocities.jp - Old Lycee TCG rules website</ExternalLink>
+                <ExternalLink href="https://lycee-tcg.com/">lycee-tcg.com - Japanese Official Lycee
+                    Overture website
+                </ExternalLink>
+            </li>
+            <li>
+                <ExternalLink href="https://web.archive.org/web/20180701222818/http://akiba.geocities.jp/lycee_tcg">
+                    akiba.geocities.jp - Old Lycee TCG rules website (archive)
+                </ExternalLink>
             </li>
             <li>
                 <ExternalLink href="https://www.facebook.com/groups/205241722843396/">
@@ -61,13 +71,13 @@
 </template>
 
 <script>
-  import ExternalLink from '../components/common/ExternalLink';
-  import NavMenu from '../components/NavMenu';
+import ExternalLink from '../components/common/ExternalLink';
+import NavMenu from '../components/NavMenu';
 
-  /** @class IndexPage */
-  export default {
-    components: { ExternalLink, NavMenu },
-  };
+/** @class IndexPage */
+export default {
+  components: { ExternalLink, NavMenu },
+};
 </script>
 
 <style scoped>
