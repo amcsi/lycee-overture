d163610ffaad3919abb744b89429a582c55e5f29
MasterCritics <54103943+MasterCritics@users.noreply.github.com>
2020-02-21 17:45:54 +0800

    Update TurnRules.vue

--- resources/js/components/rules/TurnRules.vue
+++ resources/js/components/rules/TurnRules.vue
@@ -2,170 +2,163 @@
     <section>
         <h2>Turn</h2>
 
-        <p>The turn is done in the following order:</p>
+        <p>The turn order goes as follows:</p>
 
         <el-card>
-            <h3 slot="header">Start phase</h3>
+            <h3 slot="header">Start Phase</h3>
 
             <div>
-                <h4>Wake up</h4>
+                <h4>Wake-Up:</h4>
                 <p>Untap all your characters.</p>
 
-                <h4>Warm up</h4>
-                <p>Draw 2 cards from your deck (and add them to your hand).</p>
+                <h4>Warm-Up</h4>
+                <p>Draw 2 cards from your deck and add them to your hand.</p>
             </div>
         </el-card>
 
         <el-card>
-            <h3 slot="header">Main phase</h3>
+            <h3 slot="header">Main Phase</h3>
 
             <div>
-                <h4>You can...</h4>
+                <h4>The player taking his turn can:</h4>
                 <ul>
                     <li>
-                        Use a card from your hand
+                        From own hand
                         <ul>
                             <li>Summon a character</li>
                             <li>Use an event</li>
                             <li>Equip an item</li>
                         </ul>
                     </li>
-                    <li>Use the ability of one of your characters</li>
+                    <li>Use his character abilities</li>
                     <li>Attack</li>
                 </ul>
 
-                <h4>Your opponent can...</h4>
+                <h4>The other player can:</h4>
                 <ul>
                     <li>Use events</li>
-                    <li>Use the ability of one of his characters</li>
+                    <li>Use his character abilities</li>
                 </ul>
             </div>
         </el-card>
 
         <el-card>
-            <h3 slot="header">End phase</h3>
+            <h3 slot="header">End Phase</h3>
 
             <div>
                 <h4>Hand adjustment</h4>
 
-                When you have 8 or more cards in your hand, discard cards until you have 7.
+                If you end your turn with 8 or more cards in your hand, discard until you have 7.
 
-                <p>Any changes to your characters' stats get reset.</p>
+                <p>Reset all character stat changes.</p>
             </div>
         </el-card>
 
-        <h2>Start phase</h2>
+        <h2>Start Phase</h2>
 
-        <p>The following two steps happen:</p>
+        <p>The start phase consists of the following two steps:</p>
 
-        <h3>【1. Wake up】</h3>
+        <h3>【1. Wake-Up】</h3>
 
         <p>
-            Untap all your characters (ally characters) on the field. (Make them vertical).
-            I will make all of the characters (ally characters) appearing in my field unplayed. (We will make the card
-            vertical)
+            Untap (put verticle) all ally character on your side of the field.
         </p>
+        <p>※ This is a must.</p>
 
-        <h3>【2. Warm up】</h3>
+        <h3>【2. Warm-Up】</h3>
 
         <p>Draw 2 cards from your deck and add them to your hand.</p>
-        <p>The starting player on his first turn only draws 1 card.</p>
+        <p>※ The starting player draws only 1 card on his very first turn.</p>
 
-        <h2>Main phase</h2>
+        <h2>Main Phase</h2>
 
         <p>
-            In the main phase you can make various declaration using cards in your hand and your characters on the
-            field.<br />
-            You can make these declaration any number of times and in any order you like.<br />
-            If you do not declare any more, end the main phase and move on to the end phase.
+            During the Main Phase, players are allowed to declare plays from cards in their hand or characters in their field.<br />
+            Players can declare plays as many times and in any order they like.<br />
+            When neither player are able nor willing to declare plays, the Main Phase ends and the turn moves on to the End Phase.
         </p>
 
-        <h3>Use your hand</h3>
+        <h3>[Use From Hand]</h3>
 
-        <h4>Summon characters</h4>
+        <h4>[Summon Characters]</h4>
 
-        <p>Place a character on the field.</p>
+        <p>Place characters on your field.</p>
 
         <ul>
             <li>
-                The character gets placed into the field of your choice on your side of the board.<br />
-                Specify which field the character gets summoned to during this declaration.<br />
-                Only 1 character can be in 1 field.
+                Summon characters on to a space on your side of the field.<br />
+                When declaring summon, specify the space you want your character to be summoned to.<br />
+                Each space can hold only 1 character.
             </li>
-            <li>Only 1 character with the same number can be on your side of the board at a time.</li>
+            <li>Only 1 character with the same card number can be on your field at a time.</li>
             <li>All characters are summoned untapped.</li>
             <li>
-                You cannot declare an attack or use any ability with a [T] cost with a character you summoned until your
-                next turn.<br />
-                You can still block attacks or support though.
+                Characters summoned this turn cannot Attack or use [T] Abilities until your next turn.<br />
+                However they can still block attacks and/or support.
             </li>
         </ul>
 
-        <h4>Use events</h4>
+        <h4>[Use Events]</h4>
 
-        <p>Use event cards from your hand for various effects. Event cards that are used get discarded.</p>
+        <p>You can use Event Cards from your hand for various effects. Events are discarded once used.</p>
 
         <ul>
-            <li>You can use event cards even during your opponent's turn</li>
+            <li>You can use Events during your opponent's turn.</li>
         </ul>
 
-        <h4>Use items</h4>
+        <h4>[Equip Items]</h4>
 
         <p>
-            You can equip a character on the field with an item from your hand.<br />
-            Place the item by the character you equip so your opponent could know which character is equipped.
+            You can use Item Cards from your hand and equip them on to characters on the field.<br />
+            Equipped Items should be placed under their respective characters so your opponent will know who the Item is equipped to.
         </p>
 
         <ul>
-            <li>A character can only be equipped with 1 item.</li>
-            <li>You can even equip an opponent's character.</li>
+            <li>Each character can equip only 1 Item.</li>
+            <li>You can equip Items on to opponent characters.</li>
         </ul>
 
-        <h4>Use a special ability of a character</h4>
+        <h4>[Use Character Abilities]</h4>
 
         <p>
-            You can use a special ability of a character of yours on the field.
+            You can use the abilities of characters on your field.
         </p>
 
         <ul>
             <li>
-                <CardText text="Unless otherwise stated, any ability that's [Activate] or [Trigger] cannot be used more than once per turn." />
+                <CardText text="Unless otherwise stated, [Activate][Trigger] abilities can only be used once a turn." />
             </li>
             <li>
-                <CardText text="Any ability that does not show a cost is always active ([Continuous])." />
+                <CardText text="Abilities with no cost shown are always active ([Continuous])." />
                 <br />
-                (The player can not use it, the effect is automatically and forcibly processed).
+                (Players cannot declare its use; Effects are automatic and must be resolved.)
             </li>
-            <li>Abilities can be used during your opponent's turn as well.</li>
+            <li>Character Abilities can be used during your opponent's Main Phase.</li>
         </ul>
 
-        <h4>Use a basic ability of a character</h4>
+        <h4>[Use Character Basic Abilities]</h4>
 
         <p>
-            You can use a basic ability of a character of yours on the field.
+            You can use the Basic Abilities of characters on your field.
         </p>
 
         <ul>
-            <li>Please see the {{ $t('rules.basicAbilityRulesTitle') }} section on more details about basic abilities.
+            <li>Refer to "{{ $t('rules.basicAbilityRulesTitle') }}" for more details about Basic Abilities.
             </li>
         </ul>
 
         <h4>Attack</h4>
 
         <p>
-            You can attack with any character in your AF.<br />
-            For details about attacking, see the explanation in the next section labelled "{{
-            $t('rules.battleRulesTitle') }}"
+            You can attack with characters in your AF.<br />
+            Refer to "{{ $t('rules.battleRulesTitle') }}" for details about attacking.
         </p>
 
-        <h3>End phase</h3>
+        <h3>End Phase</h3>
 
         <p>
-            Your turn is over when your main phase ends. If the number of cards in your hand is 8 or more, discard until
-            you only have 7 cards left in your hand. Also, if the character's stat value (DMG, AP, DP, SP) has changed
-            due to an effect, such as an event or ability, reset those ability values ​​to their original values.
-            Any e.g. basic abilities gained by an effect are lost.<br />
-            (They will not be lost if you are gaining from a continuous effect.)
+            The turn is over when the Main Phase ends. If your hand has 8 or more cards, discard until you are left with 7. Also, if character stat values (DMG, AP, DP, SP) have changed due to an effect (ability, Events, Items etc.), reset them to their original values. Abilities (basic etc.) gained from effects are also lost.<br />
+            (Continuous effects such as those generated from Items remain, however.)
         </p>
     </section>
 </template>
