7339cba6187fc91c06e6798519d8f1a73e3211ca
MasterCritics <54103943+MasterCritics@users.noreply.github.com>
2020-03-06 16:59:56 +0800

    Update BattleRules.vue

--- resources/js/components/rules/BattleRules.vue
+++ resources/js/components/rules/BattleRules.vue
@@ -4,184 +4,177 @@
 
         <ol>
             <li>
-                <h3>Attack Declaration</h3>
-                <p>A player declares an attack with an AF (front row) character while tapping it (turning it
-                    sideways).</p>
+                <h3>Declare Attack</h3>
+                <p>The player taking his turn declares an attack with 1 character in his AF (front row).</p>
             </li>
             <li>
-                <h3>Defending</h3>
+                <h3>Declare Defense</h3>
                 <p>
-                    The opponent player decides whether to defend or not with the DF (rear row) character in the same
-                    column as the attacking character.
+                    The opposing player decides whether or not to defend with his character in his DF (back row) in the same column as the attacking character.
                 </p>
             </li>
             <li>
-                <h3>Use support / ability / event</h3>
+                <h3>Use Support / Abilities / Events</h3>
                 <p>
-                    Aid character with support, ability, and events!
+                    Both players can use Support/Abilities/Events to aid their respective characters.
                 </p>
             </li>
             <li>
-                <h3>Battle result</h3>
+                <h3>Battle Result</h3>
                 <p>
-                    If defense was declared, the two characters battle. See lower down for details.<br />
-                    If not, your discard cards from the top of your opponent's deck equal to the attacking character's
-                    DMG.
+                    If a defender is declared, conduct battle.<br />
+                    If no defender is declared, the opponent (defending player) receives damage to his deck. The opponent player discards cards from the top of his deck equal to the DMG value of the attacking character.
                 </p>
             </li>
         </ol>
 
-        <h3>Attack declaration</h3>
+        <h3>Declaring Attack</h3>
 
         <p>
-            You can declare an attack at any time during your main phase of your turn.<br />
-            Designate 1 untapped character in your AF.
-            The designated character becomes the attacking character of the battle. Tap it (turn it sideways).
+            The player taking his turn can declare an attack anytime during his main phase.<br />
+            To do so, select 1 untapped character in your AF.
+            Tap that character. That character is now the attacking battling character.
         </p>
 
-        <h3>Defense declaration</h3>
+        <h3>Declaring Defense</h3>
 
         <p>
-            When your opponent declares an attack, you can choose to defend.<br />
-            Only an untapped character in the DF of the same column as the attacking character can defend.<br />
-            When declaring defense, tap the defending character.
-
+            If an attack was declared, the defending player can designate a defending character.<br />
+            (You can also choose not to defend)<br />
+            The defending character is the defending player's untapped DF character in the same column as the attacking character.<br />
+            Tap the defending character to declare defense.
         </p>
 
         <ul>
-            <li>If there is no untapped DF character in the same column as the attacker, the battle progresses without a
-                defender.
-            </li>
-            <li>You can choose not to defend even if you had a DF character.</li>
+            <li>If there are no suitable defending characters, the battle continues without a defender.</li>
+            <li>If there are suitable defending characters, the defending player can also choose not to defend. The battle then continues without a defender.</li>
         </ul>
 
-        <h3>Support declaration</h3>
+        <h3>Declaring Support</h3>
 
-        <p>Each player can declare "support". They can also declare events and abilities.</p>
+        <p>At this point in battle, both players can declare Support, Abilities and Events.</p>
 
-        <h4>Support</h4>
+        <h4>[Support]</h4>
 
         <p>
-            During the battle you can "support" participants of the battle by designating ally characters adjacent
-            (except diagonal) to the participating character.
+            During battle, players can Support battling characters with characters adjacent (except diagonal) to the battling characters.
         </p>
 
         <p>
-            Only the SP value of the supported character increases the stat value of the supported character.<br />
-            The rising stat value is AP if an attacker is supported, DP if a defender is supported.
+            When a support is declared, the supporting character's SP is added to the stat value of the character being supported.<br />
+            The stat value that is added is dependent on the role of the supported character during battle. If the aformentioned character is attacking, increase AP; if defending, increase DP.
         </p>
 
-        <p>Any character used for support gets tapped.</p>
+        <p>Characters used for support get tapped.</p>
 
         <ul>
-            <li>The stat value increased by support is effective until the end of the battle.</li>
-            <li>You can declare support as many times as you want during a battle.</li>
+            <li>Stat values increased by support is effective until the end of the battle.</li>
+            <li>Support can be declared as many times as desired in a single battle.</li>
         </ul>
 
-        <h4>Battle result</h4>
+        <h3>Battle Result</h3>
 
-        <p>The result of the battle differs depending on whether there's a defending character or not.</p>
+        <p>The battle resolves differently based on whether or not a defending character was declared.</p>
 
-        <h5>When there is no defending character</h5>
-        <p>Destroy cards from the top of the defending player's deck equal to the attacker's DMG.</p>
+        <h4>[No Defending Character]</h4>
+        <p>The defending player discards cards from the top of his deck equal to the DMG value of the attacking character.</p>
 
-        <h5>When there is a defending character</h5>
+        <h4>[With Defending Character]</h4>
         <p>
-            Compare the AP of each character to the other characters' DP.<br />
-            If the AP value exceeds the DP value, that character goes "down", and is discarded.
+            Compare AP and DP values of the battling characters.<br />
+            If AP is higher than DP, that character is "downed" (destroyed).
         </p>
 
         <ul>
             <li>
-                For both the attacker and the defender, compare the AP and DP.<br />
-                As a result, there's a possibility that both characters go "down", or even neither.
+                The AP and DP is compared between both attacking and defending characters.<br />
+                This means that both or even neither characters can be downed at the end of the battle.
             </li>
             <li>
-                If the AP and DP values are the same, that character does not go "down".
+                If AP and DP are equal, the character does not get downed.
             </li>
         </ul>
 
-        <h3>Battle example</h3>
+        <h3>Battle Example</h3>
 
-        <p>The result of the battle differs depending on whether there is a defending character or not.</p>
+        <p>Let's use the cards below to illustrate the multiple battle scenarios. Remember the battle resolves differently based on whether or not a defending character was declared.</p>
 
         <cloudinary-fetch-img src="https://lycee-tcg.com/rule/images/index_7_img1.png" />
 
-        <h4>[If there is a defending character]</h4>
+        <h4>[With Defending Character]</h4>
 
         <ol>
             <li>
-                A player declares an attack during his turn.<br />
-                The designated attacking character is "Rider / Ann Bonnie (AP 2, DP 1)".
+                The player taking his turn declares an attack.<br />
+                He designates "Rider / Anne Bonny (AP 2/DP 1)" as his attacking character.".
             </li>
             <li>
-                The defending player designates the defending character: "Rider / Edward Teach (AP 3, DP 0)".
+                The defending player designates his defending character, "Rider / Edward Teach (AP 3/DP 0)"..
             </li>
-            <li>Neither player declares support, abilities or events.</li>
+            <li>Neither player uses Supports / Abilities / Events.</li>
         </ol>
 
         <el-card>
-            <h5>Rider / Ann Bonnie's result</h5>
+            <h4>Result of Rider / Anne Bonny</h4>
             <p>
-                DP1 (Rider / Ann Bonnie) < AP3 (Rider / Edward Teach)<br />
-                Rider / Ann Bonnie goes down.
+                DP1 (Rider / Anne Bonny) < AP3 (Rider / Edward Teach)<br />
+                Rider / Anne Bonny is downed
             </p>
         </el-card>
 
         <el-card>
-            <h5>Rider / Edward Teach's result</h5>
+            <h4>Result of Rider / Edward Teach</h4>
             <p>
-                DP 0 (Rider / Edward Teach) < AP 2 (Rider / Ann Bonnie)<br />
-                Rider / Edward Teach goes down.
+                DP0 (Rider / Edward Teach) < AP2 (Rider / Anne Bonny)<br />
+                Rider / Edward Teach is downed
             </p>
         </el-card>
 
-        <h4>[When there is no defending character]</h4>
+        <h4>[No Defending Character]</h4>
 
         <ol>
             <li>
-                A player declares an attack during his turn.<br />
-                The designated attacking character is "Rider / Ann Bonnie (DMG 2, AP 2, DP 1)".
+                The player taking his turn declares an attack.<br />
+                He designates "Rider / Anne Bonny (DMG 2/AP 2/DP 1)" as his attacking character.
             </li>
-            <li>Defending player does not specify a character to defend.</li>
-            <li>Neither player declares support, abilities or events.</li>
-            <li>Opponent discards 2 cards from the top of his deck.</li>
+            <li>The defending player does not designate a defending character.</li>
+            <li>Neither player uses Supports/Abilities/Events.</li>
+            <li>Defending player discards 2 cards from the top of his deck (based on the attacking character's DMG value).</li>
         </ol>
 
-        <h4>[When there is support declared]</h4>
+        <h4>[With Support]</h4>
 
         <div class="card-image-with-numbered-bullets-on-side">
             <cloudinary-fetch-img src="https://lycee-tcg.com/rule/images/index_7_img3.png" />
             <ol>
                 <li>
-                    A player declares an attack during his turn.<br />
-                    The designated attacking character is "Rider / Edward Teach (AP 3, DP 0)".
+                    The player taking his turn declares an attack.<br />
+                    He designates "Rider / Edward Teach (AP 3/ DP 0)" as his attacking character.
                 </li>
                 <li>
-                    The defending player designates the defending character: "Rider / Ann Bonnie (DMG 2, AP 2, DP 1)".
+                    The defending player designates his defending character, "Rider / Anne Bonny (AP 2/DP 1)".
                 </li>
                 <li>
-                    The defending player declared support for "Mary Lead (SP 2)" which is to the left of "Rider / Ann
-                    Bonnie (AP 2, DP 1)".<br />
-                    Add the SP value (2) of "Mary Lead" to the DP of "Rider / Ann Bonnie (AP 2, DP 1)" (1 + 2 = 3).
+                    The defending player declares support with "Rider / Mary Read (SP 2)", who is located on the left of "Rider / Anne Bonny (AP 2/DP 1)".<br />
+                    Mary Read's SP2 is added to the defending Anne Bonny's DP (1 + 2 = 3).
                 </li>
             </ol>
         </div>
 
         <el-card>
-            <h5>Rider / Ann Bonnie's result</h5>
+            <h4>Result of Rider / Anne Bonny</h4>
             <p>
-                DP 3 (1 + 2) (Rider / Ann Bonnie) = AP 3 (Rider / Edward Teach)<br />
-                Rider / Anne Bonnie will not go down.<br />
-                <small>* If the values ​​of DP and AP are the same, the character does not go down.</small>
+                DP3 (Rider / Anne Bonny) = AP3 (Rider / Edward Teach)<br />
+                Rider / Anne Bonny is not downed<br />
+                <small>※ If AP and DP are equal, the character does not get downed</small>
             </p>
         </el-card>
 
         <el-card>
-            <h5>Rider / Edward Teach's result</h5>
+            <h5>Result of Rider / Edward Teach</h5>
             <p>
-                DP 0 (Rider / Edward Teach) < AP 2 (Rider / Ann Bonnie)<br />
-                Rider / Edward Teach goes down.
+                DP0 (Rider / Edward Teach) < AP2 (Rider / Anne Bonny)<br />
+                Rider / Edward Teach is downed
             </p>
         </el-card>
     </section>
