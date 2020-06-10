5aa99cb64bd0fa58b00f9a2da4b59c673c2626bb
Attila Szeremi <attila@szeremi.org>
2020-06-10 09:06:51 +0200

    Redirect www to non-www in FE as a quick fix

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -4,6 +4,10 @@
 <div id="app" class="flex-center position-ref full-height"></div>
 <script>
   window.vars = {!! json_encode($jsVars) !!};
+
+  if (window.location.hostname === 'www.lycee-tcg.eu') {
+    window.location.hostname = 'lycee-tcg.eu'
+  }
 </script>
 <script defer src="{{ mix('js/manifest.js') }}"></script>
 <script defer type="text/javascript" src="{{ mix('js/vendor.js') }}"></script>
