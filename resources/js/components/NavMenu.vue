ec0ab93df44744737f120d78a84da1a48e816b73
Attila Szeremi <attila@szeremi.org>
2021-01-03 18:31:30 +0100

    Hide incomplete Deck menu item from production

--- resources/js/components/NavMenu.vue
+++ resources/js/components/NavMenu.vue
@@ -3,27 +3,27 @@
         <el-menu-item index="/" route="/">
             <a href="/" @click.prevent>{{ $t('nav.welcome') }}</a>
         </el-menu-item>
-        <el-menu-item index="/news" route="/news">
-            <a href="/news" @click.prevent>{{ $t('nav.news') }}</a>
-        </el-menu-item>
-        <el-menu-item index="/rules" route="/rules">
-            <a href="/rules" @click.prevent>{{ $t('nav.rules') }}</a>
-        </el-menu-item>
-        <el-menu-item index="/cards" route="/cards">
-            <a href="/cards" @click.prevent>{{ $t('nav.cardList') }}</a>
-        </el-menu-item>
-        <el-menu-item index="/deck">
-            <router-link to="/deck">{{ $t('nav.deck') }}</router-link>
-        </el-menu-item>
+      <el-menu-item index="/news" route="/news">
+        <a href="/news" @click.prevent>{{ $t('nav.news') }}</a>
+      </el-menu-item>
+      <el-menu-item index="/rules" route="/rules">
+        <a href="/rules" @click.prevent>{{ $t('nav.rules') }}</a>
+      </el-menu-item>
+      <el-menu-item index="/cards" route="/cards">
+        <a href="/cards" @click.prevent>{{ $t('nav.cardList') }}</a>
+      </el-menu-item>
+      <el-menu-item v-if="isLocalEnv" index="/deck">
+        <router-link to="/deck">{{ $t('nav.deck') }}</router-link>
+      </el-menu-item>
 
-        <li class="language-links">
-            <a :href="enHref" class="language-link" :class="{ active: locale !== 'ja' }">
-                <FlagImage locale="en" />
-            </a>
-            <a :href="jaHref" class="language-link" :class="{ active: locale === 'ja' }">
-                <FlagImage locale="ja" />
-            </a>
-        </li>
+      <li class="language-links">
+        <a :href="enHref" class="language-link" :class="{ active: locale !== 'ja' }">
+          <FlagImage locale="en" />
+        </a>
+        <a :href="jaHref" class="language-link" :class="{ active: locale === 'ja' }">
+          <FlagImage locale="ja" />
+        </a>
+      </li>
 
         <el-menu-item style="float: right" index="/help-translate" route="/help-translate">
             <a href="/help-translate" @click.prevent>Help Translate</a>
@@ -54,6 +54,7 @@ function getLocaleChangeUrl(vueComponent, locale) {
       return {
         locale: window.locale,
         authDetails: window.vars.auth,
+        isLocalEnv: window.vars.environment === 'local',
       };
     },
     computed: {
