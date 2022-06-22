bae65f7cec411a383ec28a8eeded562bae58a553
Attila Szeremi <attila@szeremi.org>
2022-06-23 01:06:52 +0200

    Temporarily disable Yahoo kanji translation

--- app/I18n/NameTranslator/KanjiTranslator.php
+++ app/I18n/NameTranslator/KanjiTranslator.php
@@ -3,26 +3,19 @@ declare(strict_types=1);
 
 namespace amcsi\LyceeOverture\I18n\NameTranslator;
 
-use amcsi\LyceeOverture\I18n\TranslatorInterface;
-use JpnForPhp\Analyzer\Analyzer;
-
 /**
  * Attempts to translate kanji names to English.
  */
 class KanjiTranslator
 {
-    public function __construct(private TranslatorInterface $yahooKanjiTranslator)
+    public function __construct()
     {
     }
 
     public function translate(string $text): string
     {
-        // Only attempt to translate if the text has kanji, but does not have katakana or latin letters.
-        $hasKanjiOrHiragana = Analyzer::hasKanji($text);
-        if ($hasKanjiOrHiragana && !Analyzer::hasKatakana($text) && !Analyzer::hasLatinLetters($text)) {
-            $text = $this->yahooKanjiTranslator->translate($text);
-        }
-
+        // Temporarily disable kanji translation until we find a replacement for the Yahoo API that does not allow
+        // use outside of Japan anymore.
         return $text;
     }
 }
