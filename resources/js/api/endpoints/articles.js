8fc9972b13629dff86194b08f06c35528cadf001
Attila Szeremi <attila@szeremi.org>
2022-08-09 20:46:45 +0200

    Fix X-Requested-With CORS issue with Wordpress...
    https://stackoverflow.com/a/62724928/1381550

--- resources/js/api/endpoints/articles.js
+++ resources/js/api/endpoints/articles.js
@@ -4,6 +4,12 @@ const CATEGORY_NEWS = 2;
 
 export async function listArticles(limit) {
   return (
-    await blogApi.get('/v2/posts', { params: { categories: CATEGORY_NEWS, per_page: limit } })
+    await blogApi.get('/v2/posts', {
+      params: { categories: CATEGORY_NEWS, per_page: limit },
+      transformRequest(data, headers) {
+        delete headers.common['X-Requested-With'];
+        return data;
+      },
+    })
   ).data;
 }
