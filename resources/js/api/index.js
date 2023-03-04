bf4482cd68f2a4040abfa94e2aa8ca5d00cc1cf3
Attila Szeremi <attila@szeremi.org>
2023-03-04 18:26:12 +0100

    Forward xdebug cookie to API

--- resources/js/api/index.js
+++ resources/js/api/index.js
@@ -1,7 +1,20 @@
 import axios from 'axios';
+import { getCookie } from '../utils/browser';
 
 const api = axios.create({
   baseURL: window.vars.apiBaseUrl,
+});
+
+api.interceptors.request.use(config => {
+  // Forward xdebug/clockwork cookies to the API.
+  const xdebugSessionValue = getCookie('XDEBUG_SESSION');
+  if (!config.params) {
+    config.params = {};
+  }
+  if (xdebugSessionValue) {
+    config.params.XDEBUG_SESSION_START = xdebugSessionValue;
+  }
+  return config;
 });
 
 export default api;
---
+++ resources/js/utils/browser.js
@@ -0,0 +1,19 @@
+// https://www.w3schools.com/js/js_cookies.asp
+export function getCookie(cname) {
+  var name = cname + '=';
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return null;
+}
+
+// https://stackoverflow.com/a/61596084/1381550
+export const inIFrame = window !== window.parent;
