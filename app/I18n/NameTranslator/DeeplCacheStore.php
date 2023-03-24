48451360137710e183adb4bbe416d76af049c99a
Attila Szeremi <attila@szeremi.org>
2023-03-24 22:56:07 +0100

    Avoid "re-translating" DeepL texts even in dry runs

--- app/I18n/NameTranslator/DeeplCacheStore.php
+++ app/I18n/NameTranslator/DeeplCacheStore.php
@@ -18,19 +18,21 @@ class DeeplCacheStore
     public function rememberForever($text, callable $callback): string
     {
         $translationsBySource = $this->getAllBySource();
-        if (($deeplTranslation = $translationsBySource->get($text)) === null) {
+        if (! $translationsBySource->has($text)) {
             $translation = $callback();
-            if ($translation === null) {
-                return $text;
-            }
             $deeplTranslation = new DeeplTranslation();
             $deeplTranslation->source = $text;
             $deeplTranslation->translation = $translation;
             $translationsBySource[$text] = $deeplTranslation;
-            $deeplTranslation->save();
+            if ($translation !== null) {
+                $deeplTranslation->save();
+            }
+        } else {
+            $deeplTranslation = $translationsBySource[$text];
         }
 
-        return $deeplTranslation->translation;
+        // Translation can only be NULL in a dry run.
+        return $deeplTranslation->translation ?? $text;
     }
 
     /**
