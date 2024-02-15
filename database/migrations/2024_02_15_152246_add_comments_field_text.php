2fa37f205a3b658c7890fa4659115e90f3b14980
Attila Szeremi <attila@szeremi.org>
2024-02-15 16:27:08 +0100

    Make comments columns into text to store more characters

---
+++ database/migrations/2024_02_15_152246_add_comments_field_text.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        \Schema::table('card_translations', function (Blueprint $table) {
+            $table->text('comments')->default('')->change();
+        });
+        \Schema::table('suggestions', function (Blueprint $table) {
+            $table->text('comments')->default('')->change();
+        });
+    }
+
+    public function down(): void
+    {
+        \Schema::table('card_translations', function (Blueprint $table) {
+            $table->string('comments')->default('')->change();
+        });
+        \Schema::table('suggestions', function (Blueprint $table) {
+            $table->string('comments')->default('')->change();
+        });
+    }
+};
