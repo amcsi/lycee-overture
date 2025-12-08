744fb48660f36a7689f6549e1b2b09f6cb1c1c50
Attila Szeremi <attila@szeremi.org>
2025-12-08 20:39:18 +0100

    Emulate prepares

--- config/database.php
+++ config/database.php
@@ -19,6 +19,7 @@ $mysql = [
     'engine' => null,
     'options' => extension_loaded('pdo_mysql') ? array_filter([
         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        PDO::ATTR_EMULATE_PREPARES => true,
     ]) : [],
 ];
 
