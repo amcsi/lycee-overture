93f2a11b3c7b642062d942891a03a0c64bb18747
Attila Szeremi <attila@szeremi.org>
2025-12-16 19:49:19 +0100

    Save memory in DeeplCacheStore

--- app/I18n/NameTranslator/DeeplCacheStore.php
+++ app/I18n/NameTranslator/DeeplCacheStore.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace amcsi\LyceeOverture\I18n\NameTranslator;
 
 use amcsi\LyceeOverture\DeeplTranslation;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 
 class DeeplCacheStore
 {
@@ -12,42 +12,46 @@ class DeeplCacheStore
 
     public function get(string $text, string $locale): ?string
     {
-        return $this->getAllBySource($locale)->get($text)?->translation;
+        return $this->getAllBySource($locale)->get($text);
     }
 
     public function rememberForever($text, string $locale, callable $callback): string
     {
         $translationsBySource = $this->getAllBySource($locale);
-        if (! $translationsBySource->has($text)) {
+        $translation = $translationsBySource->get($text);
+        if (! $translation) {
+            /** @var string|null $translation */
             $translation = $callback();
             $deeplTranslation = new DeeplTranslation();
             $deeplTranslation->source = $text;
             $deeplTranslation->translation = $translation;
             $deeplTranslation->locale = $locale;
-            $translationsBySource[$text] = $deeplTranslation;
             if ($translation !== null) {
                 $deeplTranslation->save();
+                $translationsBySource->put($text, $translation);
             }
-        } else {
-            $deeplTranslation = $translationsBySource[$text];
         }
 
         // Translation can only be NULL in a dry run.
-        return $deeplTranslation->translation ?? $text;
+        return $translation ?? $text;
     }
 
     public function flush()
     {
-        $this->translationsBySource = null;
+        $this->translationsBySource = [];
     }
 
     /**
-     * @return Collection|DeeplTranslation[]
+     * @return Collection|string[]
      */
     private function getAllBySource(string $locale): Collection
     {
-        return $this->translationsBySource[$locale] ??= DeeplTranslation::where('locale', $locale)
-            ->get()
-            ->keyBy('source');
+        return $this->translationsBySource[$locale] ??= DeeplTranslation::query()
+            ->where('locale', $locale)
+            ->lazy()
+            ->mapWithKeys(function (DeeplTranslation $translation) {
+                return [$translation->source => $translation->translation];
+            })
+            ->collect();
     }
 }
